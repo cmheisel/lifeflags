4c0b64d3a4af6d74daa1d62fa1acffc13aed5cd1
Chris <chris@heisel.org>
2009-11-26 10:39:51 -0500

    Added basic common config.

---
+++ lifeflags/conf/common/settings.py
@@ -0,0 +1,31 @@
+import os
+
+from django.conf.global_settings import *
+
+PROJECT_ROOT = os.path.realpath(os.path.join(__file__, '..', '..', '..', '..'))
+
+TIME_ZONE = 'America/New_York'
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
+ROOT_URLCONF = 'lifeflags.conf.common.urls'
+
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_ROOT, 'templates')
+)
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+)
