6d9ba1223011af6241d2f8eeb9efaffa570aace7
Chris <chris@heisel.org>
2010-04-21 21:27:48 -0400

    Double saving to avoid hash collisions

--- lifeflags/flags/models.py
+++ lifeflags/flags/models.py
@@ -9,7 +9,7 @@ class Flag(models.Model):
     Represents a 'flag on the play'.
     Flag on the play. {{ Offense }}, {{ players }}. {{ Penalty }}
     """
-    slug = models.SlugField(unique=True, max_length=50)
+    slug = models.SlugField(unique=True, max_length=50, null=True)
     offense = models.CharField(max_length=140)
     players = models.CharField(max_length=140)
     penalty = models.CharField(max_length=255)
@@ -17,17 +17,11 @@ class Flag(models.Model):
     def __unicode__(self):
         return "%s-%s - %s, %s." % (self.id, self.slug, self.offense, self.players)
 
-    def __slug_seed(self):
-        """
-        Return a semi-reliably random and non-repetitive integer
-        """
-        seed = random.randint(1,10000000) + int(time.time()) #Random number + time as int
-        return base62.from_decimal(seed)
-
     def save(self, force_insert=False, force_update=False):
-        if not self.slug:
-            self.slug = "F%s" % self.__slug_seed() 
         super(Flag, self).save(force_insert, force_update)
+        if not self.slug:
+            self.slug = "F%s" % hash(self.id)
+            super(Flag, self).save(force_insert, force_update)
 
     @models.permalink
     def get_absolute_url(self):
